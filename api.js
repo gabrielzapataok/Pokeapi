b1169e2f3f4a25f2eb522f9984b279d215133f2c
gabrielzapataok <gaby.fz.58@gmail.com>
2021-02-05 17:09:42 -0300

    listo para llevar a remoto

--- api.js
+++ api.js
@@ -1,15 +1,34 @@
+//Me traigo los datos de la api
 fetch('https://pokeapi.co/api/v2/pokemon/')
+
+//los codifico a Json()
 .then(response => response.json())
 .then(data => {
+    
     console.log(data)
     let element = document.createElement('article')
     let htmlString = ''
     for (let i = 0; i < 10; i++) {
-        htmlString = htmlString + `
-        <a href="${window.location.origin}/plantilla.html?name=${data.results[i].name}">
-            <h3>
-            </h3>
-        </a>`
+
+        
+        fetch(`https://pokeapi.co/api/v2/pokemon/${data.results[i].name}`)
+        .then(response => response.json())
+        .then(data=>{
+
+        console.log(data)
+        let miElement = document.createElement('div')
+        miElement.innerHTML=`
+        <a href = "plantilla.html?pokemon=${data.name}">
+            <article>
+                <h3>nombre: ${data.name}</h3>
+                <img src="${data.sprites.front_default}"></img>
+            </article>
+        </a>
+        `
+        document.getElementById('container').append(miElement)
+        });
+
+
     }
     element.innerHTML = htmlString
     document.getElementById('container').append(element)
